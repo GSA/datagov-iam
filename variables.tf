c6bb93c295100216ac9a35f4b9d8db91a27ffc4d
Aaron D Borden <adborden@a14n.net>
2021-03-09 16:20:27 -0800

    wip

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "github_owner" {
+  description = "GitHub username who is applying changes to GitHub resources."
+  type = string
+}
+
+variable "github_token" {
+  description = "GitHub personal access token for applying GitHub resources."
+  type = string
+}
